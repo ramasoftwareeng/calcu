3b7a688e60f3c3734bd8ab8a780c5f1864273505
Rama <rama12006hind@gmail.com>
2024-08-06 18:31:24 +0300

    first of all

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+<title>
+    calculator
+</title>
+
+</svelte:head>
+<div class="bg-white  h-[400px] w-[250px] rounded-3xl">
+
+
+</div>
+
